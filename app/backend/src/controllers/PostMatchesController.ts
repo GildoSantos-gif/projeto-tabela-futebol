b5500111f4150d7ceb440eb6baffa7d669bba9fb
Seu nomecd <canaldogildosantos@gmail.com>
2022-06-06 21:04:54 -0300

    desafios 25 e 26

--- app/backend/src/controllers/PostMatchesController.ts
+++ app/backend/src/controllers/PostMatchesController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
-// import Match from '../database/models/MatchModel';
 import PostMatchesService from '../services/PostMatchesService';
+
+const message = {
+  message: 'It is not possible to create a match with two equals teams',
+};
+const message2 = { message: 'There is no team with such id!' };
 
 export default class PostMatchesController {
   postMatchesService: PostMatchesService;
-
   constructor() {
     this.postMatchesService = new PostMatchesService();
   }
@@ -13,10 +16,13 @@ export default class PostMatchesController {
     try {
       const { homeTeam, awayTeam, homeTeamGoals,
         awayTeamGoals, inProgress } = req.body;
+      if (homeTeam === awayTeam) {
+        return res.status(401).json(message);
+      }
       const result = await this.postMatchesService.createMatch(
         { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress },
       );
-      console.log(result);
+      if (result === undefined) return res.status(404).json(message2);
       return res.status(201).json(result);
     } catch (e) {
       res.status(500).json({ message: 'internal server error' });
--- app/backend/src/services/PostMatchesService.ts
+++ app/backend/src/services/PostMatchesService.ts
@@ -1,12 +1,20 @@
 import Match from '../database/models/MatchModel';
 import { createMatchType } from '../helpers/matcheType';
+import Team from '../database/models/TeamModel';
 
 export default class PostMatchesService {
   createMatch = async (data: createMatchType) => {
     const { homeTeam, awayTeam } = data;
-    if (homeTeam === awayTeam) {
+    const getHomeTeam = await Team.findOne({ where: { id: homeTeam } });
+    const getAwayTeam = await Team.findOne({ where: { id: awayTeam } });
+    // console.log(getHomeTeam, getAwayTeam); //está retornando null
+    if (!getHomeTeam || !getAwayTeam) {
       return undefined;
     }
+    if (getHomeTeam === null || getAwayTeam === null) {
+      return undefined;
+    }
+
     const result = await Match.create(data);
     return result;
   };
