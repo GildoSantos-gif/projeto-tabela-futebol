0b28a0e83776affca1b1e848266198ca2d591e3e
Seu nomecd <canaldogildosantos@gmail.com>
2022-06-07 16:26:26 -0300

    req.24

--- app/backend/src/services/PostMatchFinishService.ts
+++ app/backend/src/services/PostMatchFinishService.ts
@@ -2,6 +2,6 @@ import Match from '../database/models/MatchModel';
 
 export default class PostMatchesFinishService {
   createMatch = async (id: number) => {
-    await Match.update({ inProgress: false }, { where: { id: +id } });
+    await Match.update({ inProgress: 'false' }, { where: { id: +id } });
   };
 }
