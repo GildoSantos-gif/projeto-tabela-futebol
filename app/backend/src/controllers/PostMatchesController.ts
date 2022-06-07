b0a56a058a43c8521f4a46ac7161107059bfcbf6
Seu nomecd <canaldogildosantos@gmail.com>
2022-06-07 16:28:13 -0300

    req24

--- app/backend/src/controllers/PostMatchesController.ts
+++ app/backend/src/controllers/PostMatchesController.ts
@@ -21,7 +21,7 @@ export default class PostMatchesController {
       const getAwayTeam = await Team.findByPk(Number(awayTeam));
       if (!getHomeTeam || !getAwayTeam) return res.status(404).json(message2);
 
-      if (Number(homeTeam) === Number(awayTeam)) {
+      if (Number(homeTeam.length) === Number(awayTeam.lenght)) {
         return res.status(401).json(message);
       }
       const result = await this.postMatchesService.createMatch(
